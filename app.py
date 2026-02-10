d9e0f7748e38d87f6f24bec00fb48c1a0129c66b
chandra babu <chandrabn01@gmail.com>
2026-02-10 15:38:27 +0530

    updated with front end changes

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api', methods =['POST'])
-@app.route('/predict_api/', methods =['POST'])
 def predict_api():
     data= request.json['data']
     print(data)
@@ -24,6 +23,12 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods =['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The predicted house price is ${:.2f}".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
